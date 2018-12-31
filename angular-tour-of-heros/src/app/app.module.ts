3d313e337bd1b6880ec839a8b11b505d21e5c1ca
takuumi <satu_noone@hotmail.com>
2018-12-31 23:49:58 +0900

    heroesのスペルミスが問題だった

--- angular-tour-of-heros/src/app/app.module.ts
+++ angular-tour-of-heros/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
